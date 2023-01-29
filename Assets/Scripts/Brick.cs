f38a34242230805dc2ccd62532964cc1f744c96a
mimovnik-manjaro <jkwidzinski@gmail.com>
2023-01-29 16:00:03 +0100

    Add level 5 with moving velocity bricks

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -4,16 +4,21 @@ public class Brick : MonoBehaviour
 {
     [SerializeField] private Vector2 initialForce;
     [SerializeField] private bool willMove = false;
-    [SerializeField] private int durability = 1;
+    [SerializeField] private int maxDurability = 1;
+    [SerializeField] private Sprite brokenSprite;
     private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private int durability;
 
     public void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     public void Start()
     {
+        durability = maxDurability;
         if (!willMove)
         {
             rb.constraints = RigidbodyConstraints2D.FreezeAll;
@@ -36,6 +41,10 @@ public class Brick : MonoBehaviour
     protected virtual void takeDamage(Rigidbody2D ball)
     {
         durability--;
+        if (maxDurability > 1)
+        {
+            spriteRenderer.sprite = brokenSprite;
+        }
         if (durability <= 0)
         {
             Destroy(gameObject);
